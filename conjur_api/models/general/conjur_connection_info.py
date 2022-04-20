9ff882a57b2caa1c1ea4f22027b0367fbb6c9c7f
alonbenhorin <alon.benhorin@cyberark.com>
2022-04-20 11:50:01 +0300

    Change authn_type to be saved as int

--- conjur_api/models/general/conjur_connection_info.py
+++ conjur_api/models/general/conjur_connection_info.py
@@ -6,8 +6,8 @@ ConjurConnectionInfo module
 This module represents an object that holds the connection data
 """
 
+
 # pylint: disable=too-few-public-methods
-from conjur_api.models.enums.authn_types import AuthnTypes
 
 
 class ConjurConnectionInfo:
@@ -15,7 +15,7 @@ class ConjurConnectionInfo:
     Used for setting user input data
     """
 
-    def __init__(self, conjur_url: str = None, account: str = None, cert_file: str = None, authn_type=AuthnTypes.AUTHN):
+    def __init__(self, conjur_url: str = None, account: str = None, cert_file: str = None, authn_type: int = 0):
         self.conjur_url = conjur_url
         self.conjur_account = account
         self.cert_file = cert_file
