624e57e64a63efa573ef3bfa64681820d2ba8aa6
Shlomo Zalman Heigh <shlomo.heigh@cyberark.com>
2025-06-11 16:48:34 -0400

    Add integration test for fetching multiple secrets

--- tests/integration/test_integration_common.py
+++ tests/integration/test_integration_common.py
@@ -40,6 +40,17 @@ class TestIntegrationCommon(AsyncTestCase):
       response = await client.get('data/non-existing-variable')
     self.assertEqual(context.exception.status, 404)
 
+  async def test_integration_fetch_multiple_variables(self):
+    client = await create_admin_client()
+    await client.set('data/foo', 'test-value1')
+    await client.set('data/bar', 'test-value2')
+    await client.set('data/baz', 'test-value3')
+
+    response = await client.get_many('data/foo', 'data/bar', 'data/baz')
+    self.assertEqual(response['data/foo'], 'test-value1')
+    self.assertEqual(response['data/bar'], 'test-value2')
+    self.assertEqual(response['data/baz'], 'test-value3')
+
   @classmethod
   async def _add_test_data(cls):
     c = await create_admin_client()
